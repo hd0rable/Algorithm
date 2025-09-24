4181df058d37eb00487d8a9b3fa0d6e8b65b2eaa
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-09-24 13:43:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args){
-        // Scanner + 선택정렬
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        // BufferedReader + 선택정렬
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
        // 줄 수 입력
-       int length = sc.nextInt();
+       int length = Integer.parseInt(br.readLine());
 
         // 정수 해당 줄만큼 입력
        int [] list = new int [length];
        for (int i = 0; i < length; i++){
-           list[i] = sc.nextInt();
+           list[i] = Integer.parseInt(br.readLine());
        }
 
        // 기본 선택 정렬
-       for (int i=0; i< length-1; i++){
-           for (int j= i+1; j< length; j++ ){
+       for (int i=0; i < length-1; i++){
+           for (int j= i+1; j < length; j++ ){
                if(list[i]>list[j]){
                    int temp = list[i];
                    list[i] = list[j];
