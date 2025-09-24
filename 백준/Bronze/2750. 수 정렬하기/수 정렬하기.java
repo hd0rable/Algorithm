7a050e6d8e69311fa1ff966d9c9d0144f02fad28
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-09-24 21:57:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,31 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // BufferedReader + Arrays.sort + StringBuilder
+
+        // BufferedReader + 계수정렬(Counting Sort) + StringBuilder
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringBuilder sb = new StringBuilder();
 
        // 줄 수 입력
-       int length = Integer.parseInt(br.readLine());
+       int N = Integer.parseInt(br.readLine());
 
-        // 정수 해당 줄만큼 입력
-        int [] list = new int [length];
-       for (int i = 0; i < length; i++){
-           list[i] = Integer.parseInt(br.readLine());
+       // 범위 ~1000 <= input && input >=1000
+        // arr[1000] == 0
+       boolean[] arr = new boolean[2001];
+
+       for (int i = 0; i < N; i++){
+           arr[Integer.parseInt(br.readLine()) + 1000] = true;
        }
 
-       // 정렬 메소드
-       Arrays.sort(list);
+       // 정렬 과정이 따로 필요없음
 
+        // 중복되는 수가 없으므로 각 인덱스에 맞는 숫자를 출력하면됨 O(N)
        // 출력
-        for(int val : list){
-           sb.append(val).append("\n");
-        }
+       for(int i=0 ; i< arr.length; i++){
+           if(arr[i]){
+               sb.append(i - 1000).append("\n");
+           }
+       }
         System.out.println(sb);
     }
 }
