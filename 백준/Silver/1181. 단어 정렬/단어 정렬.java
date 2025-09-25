bcba42e9c68c756c5f27964662ad550795a3e465
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-09-25 19:24:35 +0900

    [Silver V] Title: 단어 정렬, Time: 304 ms, Memory: 24540 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,57 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-        String[] arr = new String[N];
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            arr[i] = br.readLine();
+            set.add(br.readLine()); // set으로 입력받아 중복 자동 제거
         }
 
-        // 중복 제거
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (arr[i].equals(arr[j])) {
-                    String temp = arr[j];
-                    arr[j] = arr[N - 1];
-                    arr[N - 1] = temp;
-                    N--;
-                    j--;
-                }
-            }
-        }
+        // Set -> List 변환
+        List<String> list = new ArrayList<>(set);
 
-        for (int i = 0; i < N; i++) {
-            // 최소길이를 가지고있는 스트링의 인덱스
-            int flag = i;
-            // 최소값
-            int min = arr[i].length();
-            for (int j = i + 1; j < N; j++) {
-                // 순서대로 최소값보다 작을경우 최소값 갱신
-                if(min > arr[j].length()){
-                    min = arr[j].length();
-                    flag = j;
-                }
-                // 길이가 같을경우 사전순으로
-                else if(min == arr[j].length()){
-                    flag = arr[j].compareTo(arr[flag]) < 0  ? j : flag ;
-                }
-            }
-            // 반복문이 끝나고 나면 스트링의 길이가 가장 짧은 스트링이 앞에오도록 자리바꿈
-            String temp = arr[i];
-            arr[i]= arr[flag];
-            arr[flag] = temp;
-        }
+        // 길이 오름차순, 길이 같으면 사전순으로 정렬
+        // 반환값 < 0 : a가 앞
+        // 반환값 = 0 : 순서유지
+        // 반환값 > 0 : b가 앞
+        Collections.sort(list, (a,b) -> {
+            if(a.length() != b.length())
+                return a.length() - b.length();
+            return a.compareTo(b);
+        });
 
-        for (int i = 0; i < N; i++) {
-           sb.append(arr[i]).append("\n");
+        StringBuilder sb = new StringBuilder();
+        for(String s: list){
+            sb.append(s).append("\n");
         }
         System.out.println(sb);
     }
