041c296b0ae542479559c64a15c23e48331fc558
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-09-27 23:02:00 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -7,23 +7,26 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = br.readLine();
-        String[] tokens = line.split(" ");
+        // 문자열 비교보다 단순 숫자 비교가 더 빠름
+        String line =  br.readLine();
+        String[] token = line.split(" ");
         int[] arr = new int[8];
-        for (int i = 0; i < 8; i++) {
-            arr[i] = Integer.parseInt(tokens[i]);
+        for(int i = 0; i < 8; i++){
+            arr[i] = Integer.parseInt(token[i]);
         }
 
         boolean ascending = true;
         boolean descending = true;
 
-        for (int i = 0; i < 7; i++) {
-            if (arr[i] < arr[i + 1]) descending = false;
-            else if (arr[i] > arr[i + 1]) ascending = false;
+        // Arrays.equals가 배열 전체 비교이므로, 최악 8회 비교
+        // 정수 배열선언시, 배열 한번 순회만으로도 가능하다.
+        for(int i = 0; i < 7; i++){
+            if(arr[i] > arr[i+1]) ascending = false;
+            else if(arr[i] < arr[i+1]) descending = false;
         }
 
-        if (ascending) System.out.println("ascending");
-        else if (descending) System.out.println("descending");
+        if(ascending) System.out.println("ascending");
+        else if(descending) System.out.println("descending");
         else System.out.println("mixed");
 
     }
