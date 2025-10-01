c79f0d4c96d2dbee72ec8ff240343ac4f178e0d2
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-01 22:45:24 +0900

    [Bronze IV] Title: 10부제, Time: 108 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10797. 10부제/10부제.java
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -10,11 +10,9 @@ public class Main {
         int day = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] car = new int[5];
         int count = 0;
         for (int i = 0; i < 5; i++) {
-            car[i] = Integer.parseInt(st.nextToken());
-            if (day == car[i])
+            if (day == Integer.parseInt(st.nextToken()))
                 count++;
         }
         System.out.println(count);
