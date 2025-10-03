eaff8ace1fdeeb21be2267959d21db47e63d8152
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-03 20:04:18 +0900

    [Bronze III] Title: 윷놀이, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -5,39 +5,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int[] sum = new int[3];
+        String[] arr = {"D","C","B","A","E"};
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<3; i++) {
-            st = new StringTokenizer(br.readLine());
-            sum[i] = 0;
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 4; j++) {
-                sum[i] += Integer.parseInt(st.nextToken());
+                sum += Integer.parseInt(st.nextToken());
             }
+            sb.append(arr[sum]).append("\n");
         }
-
-        // 0 1개 1 3개 도 3 A
-        // 0 2개 1 2개 개 2 B
-        // 0 3개 1 1개 걸 1 C
-        // 0 4개 윷 0 D
-        // 1 4개 모 4 E
-        for (int i=0; i<3; i++) {
-            switch (sum[i]) {
-                case 0:
-                    System.out.println("D");
-                    break;
-                case 1:
-                    System.out.println("C");
-                    break;
-                case 2:
-                    System.out.println("B");
-                    break;
-                case 3:
-                    System.out.println("A");
-                    break;
-                case 4:
-                    System.out.println("E");
-                    break;
-            }
-        }
+        System.out.println(sb);
     }
 }
