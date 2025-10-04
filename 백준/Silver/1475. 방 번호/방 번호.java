b34d8e3ebc512652318c3d1d9e52bf0958f6ab15
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-05 03:11:46 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -6,22 +6,16 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[10];
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        while(N>0){
-            int n = N%10;
-            if(arr[n]<count) //가지고 있는 세트에서 사용할수있는 숫자가 있다면 증가
-                arr[n]++;
-            else if(n==6 && arr[9]<count) //6일경우 9도 사용가능하므로 9가있다면 사용
-                arr[9]++;
-            else if(n==9 && arr[6]<count) //9일경우 6도 사용가능하므로 6이있다면 사용
-                arr[6]++;
-            else{
-                count++; //사용할수 있는 숫자가없다면 세트 수 증가
-                arr[n]++;
-            }
-            N=N/10;
+        String N = br.readLine();
+        for(int i=0; i<N.length(); i++){
+            arr[N.charAt(i)-'0']++;
         }
-        System.out.println(count);
+        arr[6] = (arr[6] + arr[9] + 1)/2; //9->6
+        arr[9] = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<10; i++){
+            max = Math.max(max,arr[i]);
+        }
+        System.out.println(max);
     }
 }
