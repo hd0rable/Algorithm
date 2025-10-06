f6a039897870acb3301b7c1b03ace50be0f80392
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-06 19:38:12 +0900

    [Silver II] Title: 에디터, Time: 560 ms, Memory: 124628 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -8,37 +8,36 @@ public class Main {
         String input = br.readLine();
         int M = Integer.parseInt(br.readLine());
 
-        // 커서를 기준으로 스택 두개로 나누기
-        Stack<Character> left = new Stack<>();
-        Stack<Character> right = new Stack<>();
+        // 커서를 기준으로 양방향 큐 두개로 나누기
+        Deque<Character> left = new ArrayDeque<>();
+        Deque<Character> right = new ArrayDeque<>();
 
         // 초기문자열은 커서 왼쪽에 넣기
         for(char c: input.toCharArray()){
-            left.push(c);
+            left.addLast(c);
         }
 
         for(int i=0; i<M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             switch (st.nextToken()) {
                 case "L":
-                    if(!left.empty()) right.push(left.pop());
+                    if(!left.isEmpty()) right.addFirst(left.removeLast());
                     break;
                 case "D":
-                    if(!right.empty()) left.push(right.pop());
+                    if(!right.isEmpty()) left.addLast(right.removeFirst());
                     break;
                 case "B":
-                    if(!left.empty()) left.pop();
+                    if(!left.isEmpty()) left.removeLast();
                     break;
                 case "P":
-                    left.push(st.nextToken().charAt(0));
+                    left.addLast(st.nextToken().charAt(0));
                     break;
             }
         }
         StringBuilder sb = new StringBuilder();
 
-        //왼쪽 스택에 있는 애들 출력하기위해 뒤집기
-        while (!left.isEmpty()) right.push(left.pop());
-        while (!right.isEmpty()) sb.append(right.pop());
+        for(char c : left) sb.append(c);
+        for(char c : right) sb.append(c);
         System.out.println(sb);
     }
 }
