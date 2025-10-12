5243639a0c5fcdaf5089fc15852878d14ca07475
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-12 18:39:41 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 26896 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -9,23 +9,19 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        int flag = 0;
+        int nextPush = 1;
         for(int i=0 ; i<n; i++){
             int cmd = Integer.parseInt(br.readLine());
-            for(int j= flag+1; j<=cmd; j++){
-                stack.push(j);
-                sb.append("+").append("\n");
-                if(j==cmd) flag = cmd; //스택에 마지막으로 넣은 수
+            while(nextPush<=cmd){
+                stack.push(nextPush++);
+                sb.append("+\n");
             }
-            if (cmd == stack.peek()){
-                stack.pop();
-                sb.append("-").append("\n");
+            if(cmd != stack.peek()){
+                System.out.println("NO");
+                return;
             }
-            else{
-                sb.delete(0, sb.length());
-                sb.append("NO").append("\n");
-                break;
-            }
+            stack.pop();
+            sb.append("-\n");
         }
         System.out.println(sb);
     }
