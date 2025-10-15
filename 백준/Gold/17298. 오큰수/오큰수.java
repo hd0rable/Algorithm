ec60a8d5602276aa493e5e36a96389aec7b103cf
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-15 23:59:37 +0900

    [Gold IV] Title: 오큰수, Time: 864 ms, Memory: 147400 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -10,25 +10,24 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] nums = new int[N];
-        int[] result = new int[N];
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
 
             while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-                result[stack.pop()] = nums[i]; //결과 배열에 해당 인덱스의 오큰수 저장
+                nums[stack.pop()] = nums[i]; //해당 인덱스의 오큰수 저장
             }
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            result[stack.pop()] = -1; //스택에 남은 인덱스들은 오큰수가없음
+            nums[stack.pop()] = -1; //스택에 남은 인덱스들은 오큰수가없음
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int r : result) {
-            sb.append(r).append(' ');
+        for (int num : nums) {
+            sb.append(num).append(' ');
         }
         System.out.println(sb);
 
