f33a48516505eb7054edf45a6c3660f2d5ffeabb
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-10-23 23:53:43 +0900

    [Silver II] Title: 쇠막대기, Time: 136 ms, Memory: 15652 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -5,18 +5,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String cmd = br.readLine();
-        Stack<Character> stack = new Stack<>();
         int result = 0;
+        int openBars = 0; // 열린 막대기 수
         char prev =' ';
         // 레이저면 앞괄호가 (
         // 바면 앞괄호가 )
-        for(char c : cmd.toCharArray()){
-            if(!stack.isEmpty() && stack.peek()=='(' && c ==')'){
-                stack.pop();
-                if(prev == '(') result += stack.size(); // 레이저일경우만 카운트 증가
-                else result += 1; // 바일 경우 마지막 조각 카운트 증가
+        for(int i=0; i<cmd.length(); i++){
+            char c = cmd.charAt(i);
+            if(c=='(')
+                openBars++;
+            else{
+                openBars--;
+                if(prev=='(') result+=openBars; //레이저 -> 열린 막대기만큼 조각 수 카운트
+                else result +=1; //바 -> 마지막 조각 수 카운트
+
             }
-            else stack.push(c);
             prev = c; //이전문자 저장
         }
         System.out.println(result);
