02baadb52cba2f35aa8e07efbd728070769ac7bf
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-11-06 23:17:46 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -6,8 +6,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         char[][] board = new char[n][n];
-        boolean[][] visited1 = new boolean[n][n];
-        boolean[][] visited2 = new boolean[n][n];
+        boolean[][] visited = new boolean[n][n];
         for (int i = 0; i < n; i++) {
             String st = br.readLine();
             for (int j = 0; j < n; j++) {
@@ -17,45 +16,41 @@ public class Main {
 
         int[] dx = new int[]{1, 0, -1, 0};
         int[] dy = new int[]{0, 1, 0, -1};
-        Queue<int[]> q1 = new LinkedList<>(); // 일반인
-        Queue<int[]> q2 = new LinkedList<>(); // 적록색맹
+        Queue<int[]> q = new LinkedList<>(); // 일반인
         int cnt1 = 0;
         int cnt2 = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (visited1[i][j])
-                    continue;
-                else {
-                    q1.offer(new int[]{i, j});
-                    visited1[i][j] = true;
-                    while (!q1.isEmpty()) { //같은글자 bfs 시작
-                        int[] cur = q1.poll();
+                if (!visited[i][j]){
+                    q.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    while (!q.isEmpty()) { //같은글자 bfs 시작
+                        int[] cur = q.poll();
                         for (int dir = 0; dir < 4; dir++) {
                             int nx = cur[0] + dx[dir];
                             int ny = cur[1] + dy[dir];
                             if (nx < 0 || nx >= n || ny < 0 || ny >= n)
                                 continue;
-                            if (visited1[nx][ny] || board[nx][ny] != board[cur[0]][cur[1]])
+                            if (visited[nx][ny] || board[nx][ny] != board[cur[0]][cur[1]])
                                 continue;
-                            q1.offer(new int[]{nx, ny});
-                            visited1[nx][ny] = true;
+                            q.offer(new int[]{nx, ny});
+                            visited[nx][ny] = true;
                         }
                     }
                     cnt1++;
                 }
             }
         }
+        visited = new boolean[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (visited2[i][j])
-                    continue;
-                else {
-                    q2.offer(new int[]{i, j});
-                    visited2[i][j] = true;
-                    while (!q2.isEmpty()) { //적록색맹 bfs 시작
-                        int[] cur = q2.poll();
+                if (!visited[i][j]) {
+                    q.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    while (!q.isEmpty()) { //적록색맹 bfs 시작
+                        int[] cur = q.poll();
                         for (int dir = 0; dir < 4; dir++) {
                             int nx = cur[0] + dx[dir];
                             int ny = cur[1] + dy[dir];
@@ -64,10 +59,10 @@ public class Main {
                             boolean sameColor = (board[nx][ny] == board[cur[0]][cur[1]]) ||
                                     ((board[nx][ny] == 'R' || board[nx][ny] == 'G') && (board[cur[0]][cur[1]] == 'R'
                                             || board[cur[0]][cur[1]] == 'G'));
-                            if (visited2[nx][ny] || !sameColor)
+                            if (visited[nx][ny] || !sameColor)
                                 continue;
-                            q2.offer(new int[]{nx, ny});
-                            visited2[nx][ny] = true;
+                            q.offer(new int[]{nx, ny});
+                            visited[nx][ny] = true;
                         }
                     }
                     cnt2++;
