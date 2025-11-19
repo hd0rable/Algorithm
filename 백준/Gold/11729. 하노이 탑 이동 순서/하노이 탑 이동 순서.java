a49dcca6fcbd5855cf5db2df43b572b7c9d528c7
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-11-20 00:14:51 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 396 ms, Memory: 46208 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        sb.append((int)Math.pow(2,n)-1).append("\n");
+        func(1,3,n);
+        System.out.println(sb);
+    }
+
+   static void func(int a, int b, int n){
+        if(n==1){
+            sb.append(a).append(' ').append(b).append('\n');
+            return;
+        }
+        func(a,6-a-b,n-1);
+        sb.append(a).append(' ').append(b).append('\n');
+        func(6-a-b,b,n-1);
+   }
+}
