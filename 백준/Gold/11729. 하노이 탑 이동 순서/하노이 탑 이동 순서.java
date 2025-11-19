d6781548c296a8d924c3de77d2866e2041761ba1
강희진 <kanghuijin12@gmail.com>
2025-11-20 00:32:14 +0900

    [update] 다른 풀이 업데이트

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -8,17 +8,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         sb.append((int)Math.pow(2,n)-1).append("\n");
-        func(1,3,n);
+        hanoi(n,1,2,3);
         System.out.println(sb);
     }
 
-   static void func(int a, int b, int n){
+    // n: 원판의 개수,start: 출발지, mid: 옮기기위해 이동해야할 장소, to: 목적지
+    static void hanoi(int n, int start, int mid, int to){
         if(n==1){
-            sb.append(a).append(' ').append(b).append('\n');
+            sb.append(start).append(' ').append(to).append('\n');
             return;
         }
-        func(a,6-a-b,n-1);
-        sb.append(a).append(' ').append(b).append('\n');
-        func(6-a-b,b,n-1);
-   }
+
+        // A -> C로 옮긴다고 가정할 떄,
+        // STEP 1 : N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
+        hanoi(n-1,start,to,mid);
+
+        // STEP 2 : 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 to지점으로 옮긴다.)
+        sb.append(start).append(' ').append(to).append('\n');
+
+        // STEP 3 : N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 to 지점으로 옮긴다.)
+        hanoi(n-1,mid,start,to);
+    }
 }
