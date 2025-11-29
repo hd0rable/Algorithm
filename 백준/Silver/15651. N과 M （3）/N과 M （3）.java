5a4c90500a842be8757ad6ceaf0f02d9b94f123e
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-11-29 23:02:45 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 68172 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb;
+    static int n,m;
+    static int[] arr = new int[10];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        func(0);
+        System.out.println(sb);
+    }
+
+    static void func(int k){
+        if(k==m){
+            for(int i=0; i<m; i++)
+                sb.append(arr[i]).append(' ');
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            arr[k] = i;
+            func(k+1);
+        }
+    }
+
+}
