62a2cec65c9a500ed08807fa3fdcda4e74a3cb7f
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-12-17 00:50:41 +0900

    [Silver III] Title: 시리얼 번호, Time: 112 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = br.readLine();
+        }
+        Arrays.sort(arr, (a1,a2) -> {
+            // 길이 오름차순 (짦은것이 먼저)
+            if(a1.length() != a2.length())
+                return a1.length() - a2.length();
+            // 자리수의 합 오름차순
+            else{
+                int sum1 =0, sum2 =0;
+                for(char c : a1.toCharArray()){
+                    if(c>='0'&& c<='9')
+                        sum1 += c -'0';
+                }
+                for(char c : a2.toCharArray()){
+                    if(c>='0'&& c<='9')
+                        sum2 += c -'0';
+                }
+                if(sum1 != sum2)
+                    return sum1 - sum2;
+                //String의 기본 사전순 정렬
+                else return a1.compareTo(a2);
+            }
+        });
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+}
