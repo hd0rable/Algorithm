ba1e0510f0eb2f3d117a8d7a6dd2d6da1d026b69
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-12-19 00:42:53 +0900

    [Silver III] Title: 빈도 정렬, Time: 112 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -8,27 +8,24 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-
-        ArrayList<Integer> list = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>(); // num,cnt 숫자,빈도횟수
-        ArrayList<Integer> index = new ArrayList<>();
+        
+        Map<Integer, Integer> map = new LinkedHashMap<>(); // num,cnt 숫자,빈도횟수
+        // LinkedHashMap으로 순서 보장
 
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
-            index.add(list.get(i));
-            map.put(list.get(i),map.getOrDefault(list.get(i),0)+1);
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num,map.getOrDefault(num,0)+1);
         }
 
-        Collections.sort(list,(o1,o2) ->{
-            if(map.get(o1) == map.get(o2)) //빈도수가 같을 경우 원래 입력순서 유지
-                return index.indexOf(o1) - index.indexOf(o2);
-            return Integer.compare(map.get(o2),map.get(o1)); //빈도수가 다를 경우 빈도수 기준으로 내림차순 정렬
-        });
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list,(o1,o2) -> map.get(o2) - map.get(o1));
 
         StringBuilder sb = new StringBuilder();
-        for(int i : list)
-            sb.append(i).append(' ');
+        for(int key : list) {
+            for(int i=0; i<map.get(key); i++)
+            sb.append(key).append(' ');
+        }
         System.out.println(sb);
     }
 
