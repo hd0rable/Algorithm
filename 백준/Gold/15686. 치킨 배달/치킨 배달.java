7d3f87293c8b0242b4876336b89abec57b141a22
강희진 <kanghuijin12@gmail.com>
2025-12-20 02:42:57 +0900

    [update] 다른풀이 업데이트

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -7,6 +7,7 @@ public class Main {
     static int min = Integer.MAX_VALUE;
     static int[][] arr;
     static ArrayList<int[]> chickenHousePos = new ArrayList<>();
+    static ArrayList<int[]> selectCH = new ArrayList<>();
     static ArrayList<int[]> housePos = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
@@ -22,7 +23,7 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 if(arr[i][j] == 1) housePos.add(new int[]{i,j});
                 if(arr[i][j] == 2)
-                    chickenHousePos.add(new int[]{i,j,0}); //치킨집 좌표 x,y 선택되었는지 여부(0,1)
+                    chickenHousePos.add(new int[]{i,j}); //치킨집 좌표 x,y
             }
         }
         bt(0,0);
@@ -37,30 +38,24 @@ public class Main {
         }
 
         for(int i=at; i<chickenHousePos.size(); i++){
-            int[] cur = chickenHousePos.get(i);
-            int x = cur[0]; int y = cur[1];
-            chickenHousePos.set(i,new int[]{x,y,1}); // 선택 표시
+            selectCH.add(chickenHousePos.get(i)); // 선택
             bt(selectCnt+1,i+1);
-            chickenHousePos.set(i,new int[]{x,y,0}); // 선택 해제
+            selectCH.remove(chickenHousePos.get(i)); // 선택 해제
         }
     }
 
     static int calculateDistanceSum(){
         int distanceSum = 0;
-        for(int i = 0; i <housePos.size(); i++){
-            int[] ch = housePos.get(i);
+        for (int[] ch : housePos) {
             int hx = ch[0]; int hy = ch[1];
             int minDistance = Integer.MAX_VALUE;
-            for(int j = 0; j <chickenHousePos.size(); j++){ //선택된 치킨집들중에서 최소 거리 구하기
-                int[] cch = chickenHousePos.get(j);
-                int cx = cch[0]; int cy = cch[1]; int isSelected = cch[2];
-                if(isSelected == 0) continue;
-                int result = Math.abs((hx+1)-(cx+1)) + Math.abs((hy+1)-(cy+1));
-                minDistance = Math.min(minDistance,result);
+            for (int[] cc : selectCH) { //선택된 치킨집들중에서 최소 거리 구하기
+                int cx = cc[0]; int cy = cc[1];
+                int result = Math.abs(hx - cx) + Math.abs(hy - cy);
+                minDistance = Math.min(minDistance, result);
             }
             distanceSum += minDistance;
         }
         return distanceSum;
     }
-
 }
