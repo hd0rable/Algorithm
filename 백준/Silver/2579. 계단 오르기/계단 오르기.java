73349456fdbc879d4eeb3f0040cfb6ea85e5bf99
강희진 <kanghuijin12@gmail.com>
2025-12-22 02:32:05 +0900

    [update] 최댓값으로 일차원 배열 활용 - 다른풀이 업데이트

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,25 +7,19 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] score = new int[n+1];
 
-        int total = 0;
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<=n; i++)
             score[i] = Integer.parseInt(br.readLine());
-            total += score[i];
-        }
-        if(n<=2) {
-            System.out.println(total);
-            return;
-        }
 
-        int[] dp = new int[n+1]; //i번째 계단까지 올라섰을 때 밟지 않을 계단의 합의 최솟값, i번째 계단은 반드시 밟지 않을 계단으로 선택
+        int[] dp = new int[n+1]; //i번째 계단까지 올라섰을 때 점수합의 최댓값, i번째 계단은 반드시 밟아야함
         dp[1] = score[1];
-        dp[2] = score[2];
-        dp[3] = score[3];
+        if(n>1) dp[2] = score[1] + score[2];
+        if(n>2) dp[3] = Math.max(score[1],score[2]) + score[3];
 
-        for (int i = 4; i <= n-1; i++)
-            dp[i] = Math.min(dp[i-2],dp[i-3]) + score[i]; //i계단을 안밟을거면 i-2,i-3계단중 하나를 안밟아야함
+        for (int i = 4; i <= n; i++)
+            dp[i] = score[i] + Math.max(score[i-1]+dp[i-3],dp[i-2]);
+        // 전 계단을 밟고 온 경우 score[i-1] + dp[i-3]
+        // 전 계단을 밟지 않은 경우 dp[i-2]
 
-        System.out.println(total - Math.min(dp[n-1],dp[n-2]));
-        //마지막 도착계단은 반드시 밟아야하므로 n-1,n-2중에 최소인계단을 밟지않음
+        System.out.println(dp[n]);
     }
 }
