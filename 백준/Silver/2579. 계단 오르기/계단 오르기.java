adc706c2f7664a48d46f550b66740804ed5f54d2
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-12-22 02:19:35 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,22 +7,25 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] score = new int[n+1];
 
-        for(int i=1; i<=n; i++)
+        int total = 0;
+        for(int i=1; i<=n; i++) {
             score[i] = Integer.parseInt(br.readLine());
-        if(n==1) {
-            System.out.println(score[1]);
+            total += score[i];
+        }
+        if(n<=2) {
+            System.out.println(total);
             return;
         }
 
-        int[][] dp = new int[n+1][2]; //j+1개의 계단을 연속으로 밟고 i번째 계단까지 올랐을때 점수합의 최댓값
-        dp[1][0] = score[1]; dp[1][1] = 0;
-        dp[2][0] = score[2]; dp[2][1] = score[1] + score[2];
+        int[] dp = new int[n+1]; //i번째 계단까지 올라섰을 때 밟지 않을 계단의 합의 최솟값, i번째 계단은 반드시 밟지 않을 계단으로 선택
+        dp[1] = score[1];
+        dp[2] = score[2];
+        dp[3] = score[3];
 
-        for (int i = 3; i <= n; i++) {
-            dp[i][0] = Math.max(dp[i-2][0],dp[i-2][1]) + score[i]; //1개의 계단으로 연속으로밟음 -> 전단계에서 건너뛰어밟음
-            dp[i][1] = dp[i-1][0] + score[i]; //연속으로 계단을 밟음 -> 그전계단의 최댓값+
-        }
+        for (int i = 4; i <= n-1; i++)
+            dp[i] = Math.min(dp[i-2],dp[i-3]) + score[i]; //i계단을 안밟을거면 i-2,i-3계단중 하나를 안밟아야함
 
-        System.out.println(Math.max(dp[n][0],dp[n][1]));
+        System.out.println(total - Math.min(dp[n-1],dp[n-2]));
+        //마지막 도착계단은 반드시 밟아야하므로 n-1,n-2중에 최소인계단을 밟지않음
     }
 }
