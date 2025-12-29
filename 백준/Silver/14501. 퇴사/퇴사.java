169abdf4e9f712c2894d880798ef657bf9a72e84
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-12-30 00:20:48 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] t = new int[n];
+        int[] p = new int[n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n+1]; //i일째 되는 날까지 얻을수있는 최대 수익
+
+        for(int i=0; i<n; i++){
+            if(i + t[i] <= n) //상담하는 경우
+                dp[i+t[i]] = Math.max(dp[i+t[i]],dp[i]+p[i]); //최대값 갱신
+            dp[i+1] = Math.max(dp[i+1],dp[i]); //상담하지 않는 경우 다음날로 수익 반영하기
+        }
+        System.out.println(dp[n]);
+    }
+}
