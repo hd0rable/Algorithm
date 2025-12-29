f4f5f5d4ce9ad097f5b87164adafa1aef84a7a0c
강희진 <131003143+hd0rable@users.noreply.github.com>
2025-12-30 00:41:13 +0900

    [Gold V] Title: 퇴사 2, Time: 644 ms, Memory: 304168 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -8,21 +8,20 @@ public class Main {
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
 
-        int[] t = new int[n];
-        int[] p = new int[n];
+        int[] dp = new int[n+1]; //i일째 되는 날까지 얻을수있는 최대 수익
+        int max = 0;
 
         for(int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine());
-            t[i] = Integer.parseInt(st.nextToken());
-            p[i] = Integer.parseInt(st.nextToken());
-        }
-        int[] dp = new int[n+1]; //i일째 되는 날까지 얻을수있는 최대 수익
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<n; i++){
-            if(i + t[i] <= n) //상담하는 경우
-                dp[i+t[i]] = Math.max(dp[i+t[i]],dp[i]+p[i]); //최대값 갱신
-            dp[i+1] = Math.max(dp[i+1],dp[i]); //상담하지 않는 경우 다음날로 수익 반영하기
+            max = Math.max(max, dp[i]);
+
+            if(i+t <=n)
+                dp[i+t] = Math.max(dp[i+t],max + p);
+
         }
-        System.out.println(dp[n]);
+        System.out.println(Math.max(max, dp[n]));
     }
 }
