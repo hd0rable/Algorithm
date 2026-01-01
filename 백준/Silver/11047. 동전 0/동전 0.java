e8da585a48eed2cfe3d15dad19bea8f05f9bb9e4
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-01-01 22:40:29 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -17,14 +17,8 @@ public class Main {
 
         int cnt = 0;
         for(int i=n-1; i>=0; i--){
-            if(arr[i] > k) continue;
-            while(true){
-                if(k - arr[i] >= 0){
-                    k-= arr[i];
-                    cnt++;
-                }
-                else break;
-            }
+            cnt += k / arr[i];
+            k %= arr[i];
         }
         System.out.println(cnt);
     }
