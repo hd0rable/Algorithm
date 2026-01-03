1d7a2c3a39936c8973071bd707db1e3551c8b130
강희진 <kanghuijin12@gmail.com>
2026-01-04 03:36:23 +0900

    [update] StringTokenizer 풀이 업데이트

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -4,21 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
 
         // - 기준으로 먼저 나누기
-        String[] str = line.split("-");
+        StringTokenizer str = new StringTokenizer(br.readLine(), "-");
 
-        int min = 0;
-        for(int i=0; i<str.length; i++) {
-            int sum = 0;
-            String[] tmp = str[i].split("\\+");
-            for(String num : tmp){
-                sum += Integer.parseInt(num);
+        int sum = Integer.MIN_VALUE;
+        while(str.hasMoreTokens()){
+            int tmp = 0;
+            StringTokenizer add = new StringTokenizer(str.nextToken(), "+");
+
+            while(add.hasMoreTokens()){
+                tmp += Integer.parseInt(add.nextToken());
             }
-            if(i==0) min += sum;
-            else min -= sum;
+
+            if(sum == Integer.MIN_VALUE) sum = tmp;
+            else sum -= tmp;
         }
-        System.out.println(min);
+        System.out.println(sum);
     }
 }
