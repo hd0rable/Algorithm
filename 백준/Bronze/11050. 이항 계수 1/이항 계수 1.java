0057fef6f671c5270031bcf600d29013c0a94831
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-01-12 02:47:17 +0900

    [Bronze I] Title: 이항 계수 1, Time: 92 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int result = 1;
+        for(int i=1; i<=n; i++) result*=i;
+        for(int i=1; i<=(n-k); i++) result/=i;
+        for(int i=1; i<=k; i++) result/=i;
+
+        System.out.println(result);
+    }
+}
