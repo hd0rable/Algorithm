176ab4d5c304478daa115ed2cf9886c8a9dca5eb
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-01-12 03:01:23 +0900

    [Silver II] Title: 이항 계수 2, Time: 116 ms, Memory: 18284 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int mod = 10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n+1][n+1];
+        for(int i=1; i<=n; i++){
+            dp[i][0] = dp[i][i] = 1;
+            for(int j=1; j<i; j++)
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%mod;
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
