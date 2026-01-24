eb3fb54d088647b000c107a10d06f05f33d64823
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-01-25 01:24:50 +0900

    [Silver II] Title: 좌표 압축, Time: 1880 ms, Memory: 203580 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,29 @@
+//https://www.acmicpc.net/short/status/18870
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int[] a = new int[n];
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+            set.add(a[i]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        for(int i=0; i<n; i++){
+            sb.append(Collections.binarySearch(list,a[i])).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+}
