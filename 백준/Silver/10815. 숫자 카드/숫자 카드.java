b9623d530ccf51e5c58381bcc2aec5f0520d40fb
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-02-01 23:49:51 +0900

    [Silver V] Title: 숫자 카드, Time: 684 ms, Memory: 116124 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,4 +1,3 @@
-//https://www.acmicpc.net/problem/10815
 import java.io.*;
 import java.util.*;
 
@@ -8,18 +7,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        boolean[] arr = new boolean[20000001];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[Integer.parseInt(st.nextToken())+10000000] = true;
 
-        Arrays.sort(arr);
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         while(m-- > 0){
             int num = Integer.parseInt(st.nextToken());
-            if(Arrays.binarySearch(arr,num)>=0) sb.append(1).append(' ');
+            if(arr[num+10000000]) sb.append(1).append(' ');
             else sb.append(0).append(' ');
         }
 
