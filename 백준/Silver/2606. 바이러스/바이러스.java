651cb993d918eac35fb5bab556a31cb908bcbbb7
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-03-19 00:10:34 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,15 +1,18 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static boolean[] isVisited;
+    static ArrayList<Integer>[] list;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer>[] list = new ArrayList[n+1];
+        list = new ArrayList[n+1];
         for(int i=1; i<=n; i++)
             list[i] = new ArrayList<>();
-        boolean[] isVisited = new boolean[n+1];
+        isVisited = new boolean[n+1];
         int v1,v2;
         StringTokenizer st;
         while(m-->0){
@@ -20,20 +23,16 @@ public class Main {
             list[v2].add(v1);
         }
 
-        int cnt = -1;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        isVisited[1] = true;
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            cnt++;
-            for(int next : list[cur]){
-                if(isVisited[next]) continue;
-                q.offer(next);
-                isVisited[next] = true;
-            }
-        }
+        dfs(1);
         System.out.print(cnt);
     }
 
+    static void dfs(int cur){
+        isVisited[cur] = true;
+        for(int next : list[cur]){
+            if(isVisited[next]) continue;
+            cnt++;
+            dfs(next);
+        }
+    }
 }
