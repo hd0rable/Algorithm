1dd8b67f779980b1657b0ee698d764094f22d9a5
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-03-21 23:47:02 +0900

    [Silver I] Title: 경로 찾기, Time: 248 ms, Memory: 28700 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int[][] arr;
+    static boolean[] isVisited;
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+        isVisited = new boolean[n];
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                sb.append(dfs(i,j)).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+
+    }
+
+    static int dfs(int st, int tar){
+        isVisited = new boolean[n];
+        Stack<Integer> stk = new Stack<>();
+        stk.push(st);
+        while(!stk.isEmpty()){
+            int cur = stk.pop();
+
+            if(isVisited[cur]) continue;
+            if(cur!=st) isVisited[cur] = true;
+
+            for(int nxt = 0; nxt <n; nxt++){
+                if(isVisited[nxt]) continue;
+                if(arr[cur][nxt]==1) {
+                    if(nxt == tar) return 1;
+                    stk.push(nxt);
+                }
+            }
+        }
+        return 0;
+    }
+}
