e3a674bfc93e310ce9aa3499241d388ad46bbc2a
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-03-22 00:02:06 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 17248 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,16 +1,11 @@
-//https://www.acmicpc.net/problem/11403
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int[][] arr;
-    static boolean[] isVisited;
-    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n][n];
-        isVisited = new boolean[n];
+        int[][] arr = new int[n][n];
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<n; j++){
@@ -18,35 +13,22 @@ public class Main {
             }
         }
 
+        for(int k=0; k<n; k++){ //중간
+            for(int i=0; i<n; i++){ //시작
+                for(int j=0; j<n; j++){ //도착
+                    if(arr[i][k] == 1 && arr[k][j] ==1)
+                        arr[i][j] = 1; //i->k,k->j로 가는 경로가있다면 i->j 경로도 존재
+                }
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                sb.append(dfs(i,j)).append(' ');
+                sb.append(arr[i][j]).append(' ');
             }
             sb.append('\n');
         }
         System.out.print(sb);
-
-    }
-
-    static int dfs(int st, int tar){
-        isVisited = new boolean[n];
-        Stack<Integer> stk = new Stack<>();
-        stk.push(st);
-        while(!stk.isEmpty()){
-            int cur = stk.pop();
-
-            if(isVisited[cur]) continue;
-            if(cur!=st) isVisited[cur] = true;
-
-            for(int nxt = 0; nxt <n; nxt++){
-                if(isVisited[nxt]) continue;
-                if(arr[cur][nxt]==1) {
-                    if(nxt == tar) return 1;
-                    stk.push(nxt);
-                }
-            }
-        }
-        return 0;
     }
 }
