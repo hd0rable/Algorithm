36cd7a1ce4e3100cbf15032e1e566cf12e8f1f42
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-03-29 23:37:04 +0900

    [Bronze III] Title: 웰컴 키트, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,25 +1,24 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int t;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-        int xl = Integer.parseInt(st.nextToken());
-        int xxl = Integer.parseInt(st.nextToken());
-        int xxxl = Integer.parseInt(st.nextToken());
+        int[] sizeArr = new int[6];
+        for(int i=0; i<6; i++)
+            sizeArr[i] = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        t = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
 
-        int tCount = counting(s) + counting(m) + counting(l) +
-                counting(xl) + counting(xxl) + counting(xxxl);
+        int tCount = 0;
+        for(int i=0; i<6; i++){
+            tCount += sizeArr[i]/t;
+            tCount = sizeArr[i]%t == 0 ? tCount : tCount+1;
+        }
         System.out.println(tCount);
 
         System.out.print(n/p);
@@ -27,10 +26,4 @@ public class Main {
         System.out.print(n%p);
 
     }
-
-    static int counting(int size){
-        if(size%t==0) return size/t;
-        else return size/t+1;
-    }
-
 }
