faa7919e4b73e8df3352c402735db0e9a043dffe
강희진 <131003143+hd0rable@users.noreply.github.com>
2026-05-17 19:21:26 +0900

    [level 2] Title: 올바른 괄호, Time: 6.18 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,14 @@
+import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') stack.push(c);
+            else if (!stack.isEmpty()) stack.pop();
+            else return false;
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
     }
 }
